7b88313fa0224a794dd5e4ac07da9adc949e5f53
delso-ferreira <delsofelipe@gmail.com>
2023-04-13 15:55:20 -0300

    att req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Main from './components/main';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Main />
+      </div>
+    );
   }
 }
 
