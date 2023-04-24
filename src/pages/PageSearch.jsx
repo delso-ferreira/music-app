09f8d3c6b1fc224094f48f7bb1106dd62fc68560
delso-ferreira <delsofelipe@gmail.com>
2023-04-24 15:37:44 -0300

    wip req 5

--- src/pages/PageSearch.jsx
+++ src/pages/PageSearch.jsx
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class PageSearch extends Component {
+  state = {
+    searchButtonDisabled: true,
+    name: '',
+  };
+
+  handleSearch = () => {
+    const { name } = this.state;
+
+    const maxNumber = 2;
+    const minLength = name.length >= maxNumber;
+
+    this.setState({
+      searchButtonDisabled: !minLength,
+    });
+
+    handleInputChange = ({ target: { value } }) => {
+      this.setState({
+        [name]: value,
+      }, this.handleSearch);
+    };
+  };
+
   render() {
+    const { searchButtonDisabled, name } = this.state;
+
     return (
       <>
         <Header />
         <div data-testid="page-search">PageSearch</div>
+        <form>
+          <label htmlFor="artista">
+            Artista
+          </label>
+          <input
+            type="text"
+            name={ name }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            name="searchButtonDisabled"
+            data-testid="search-artist-input"
+            disabled={ searchButtonDisabled }
+          >
+            Buscar
+          </button>
+        </form>
       </>
     );
   }
